a2ffb571d7acde97bc798cc7f44f0b8ba2e154e6
Kshitij Gupta <kshitijqwerty@gmail.com>
2021-11-29 01:17:34 +0530

    File reorganization

--- email_app-bhupendra/home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from home.models import Observer,Tasks,Participant,Recording
-admin.site.register(Observer)
-admin.site.register(Tasks)
-admin.site.register(Participant)
-admin.site.register(Recording)
