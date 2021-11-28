261fc14b35ccd0a5c1d960c92a787502779fd312
vishpandey <vishpandey2012@gmail.com>
2021-11-28 22:17:13 +0530

    adding UI

--- email_app-bhupendra/home/admin.py
+++ email_app-bhupendra/home/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from home.models import participants
-admin.site.register(participants)
+from home.models import Observer, Tasks, Participants, Recordings, Emails 
+admin.site.register(Observer)
+admin.site.register(Tasks)
+admin.site.register(Participants)
+admin.site.register(Recordings)
+admin.site.register(Emails)
