1816106fcda80b6e962d85a28ab50fdcaa4bac72
bhupendra-sharma <89186224+bhupendra-sharma@users.noreply.github.com>
2021-11-29 01:02:56 +0530

    Delete BKMPV-Mobile usability testing directory

--- BKMPV-Mobile usability testing/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from home.models import Observer, Tasks, Participants, Recordings, Emails 
-admin.site.register(Observer)
-admin.site.register(Tasks)
-admin.site.register(Participants)
-admin.site.register(Recordings)
-admin.site.register(Emails)
